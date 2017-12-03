079cfc23596872166dfb677b1f0d5eb506e251b8
shwetzpatil <shwetzpatil@gmail.com>
2017-12-03 16:23:40 +0000

    4.3 order cobtroller created

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,8 @@ Rails.application.routes.draw do
   get 'simple_pages/contact'
 
   get 'simple_pages/index'
-  root 'simple_pages#index'
+  root 'simple_pages#landing_page'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+ resources :orders, only: [:index, :show, :create, :destoy]
 end
