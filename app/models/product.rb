4688cd78783a44b8791cefe274b90a85d7fbcb77
shwetzpatil <shwetzpatil@gmail.com>
2017-12-16 12:18:12 +0000

    5.2 dynamic form edited

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,9 +1,9 @@
 class Product < ApplicationRecord
   def self.search(search_term)
     if Rails.env.production?
-      Product.where("name LIKE ?", "%#{search_term}%")
+      Product.where("name ILIKE ?", "%#{search_term}%")
     else
-      Product.where("name ilike ?", "%#{search_term}%")
+      Product.where("name LIKE ?", "%#{search_term}%")
     end
   end
 end
