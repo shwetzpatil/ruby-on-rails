45b4c7c89471ff087edf2a322dde299a5e33787a
shwetzpatil <shwetzpatil@gmail.com>
2017-12-27 13:00:33 +0000

    5.10 admin role added

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -3,6 +3,12 @@ class Ability
 
   def initialize(user)
     user ||= User.new # guest user (not logged in)
-    can :manage, User, id: user.id
+    can :read, :all  # permissions for every user, even if not logged in    
+    if user.present?  # additional permissions for logged in users (they can manage their comments)
+      can :manage, User, id: user.id 
+      if user.admin?  # additional permissions for administrators
+        can :manage, :all
+      end
+    end
   end
 end
