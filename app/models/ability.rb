273dde2c65327871542cb055f6b795a9ed608ad7
shwetzpatil <shwetzpatil@gmail.com>
2017-12-28 09:25:39 +0000

    authorization added to products and comments using cancancan gem

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -5,7 +5,10 @@ class Ability
     user ||= User.new # guest user (not logged in)
     can :read, :all  # permissions for every user, even if not logged in    
     if user.present?  # additional permissions for logged in users (they can manage their comments)
-      can :manage, User, id: user.id 
+      can :manage, User, id: user.id
+      can :create, Comment
+      cannot :crud, Product
+      
       if user.admin?  # additional permissions for administrators
         can :manage, :all
       end
