f80e5e3d1aa2b61cc73855c131a0884f4ffc9112
shwetzpatil <shwetzpatil@gmail.com>
2018-01-08 19:13:03 +0000

    6.3 function test

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -3,8 +3,8 @@ class Ability
 
   def initialize(user)
     user ||= User.new # guest user (not logged in)
-    can :read, :all  # permissions for every user, even if not logged in    
-    if user.present?  # additional permissions for logged in users (they can manage their comments)
+    can :read, [Product, Comment]  # permissions for every user, even if not logged in    
+    if user.id?  # additional permissions for logged in users (they can manage their comments)
       can :manage, User, id: user.id
       can [:create, :delete, :update, :destroy], Comment, user_id: user.id
       cannot :crud, Product
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -2,7 +2,9 @@ require 'rails_helper'
 
 describe UsersController, type: :controller do
   before do
-    @user = User.create!(first_name: "abc", last_name: "xyz", email: "abc@gmail.com", password: "123456")
+    @user_1 = User.create!(first_name: "abc", last_name: "xyz", email: "abc@gmail.com", password: "123456")
+    @user_2 = User.create!(first_name: "efg", last_name: "lmn", email: "xyz@gmail.com", password: "123456")
+    
   end
 
   describe 'GET #show' do
@@ -10,24 +12,34 @@ describe UsersController, type: :controller do
     context 'when user is logged in' do
 
       before do
-        sign_in @user
+        sign_in @user_1
       end
 
       it 'loads correct user details' do
-        get :show
-        expect(response).to be_ok
-        expect(assigns(:user)).to eq user
+        get :show, params: { id: @user_1.id }
+        expect(response).to be_success
+        expect(response).to have_http_status(200)
+        expect(assigns(:user)).to eq @user_1
+      end
+
+      it 'other users profile restricted' do
+        get :show, params: { id: @user_2.id }
+        expect(response).to redirect_to(root_path)
       end
 
     end
 
     context 'when user is logged out' do
 
-      it 'redirects to login' do
-        get :show, params: { id: user.id }
-        expect(response).to redirect_to(new_user_session_path)
-      end  
+      before do
+        sign_out @user_1
+      end
       
+      it 'redirects to login' do
+        get :show, params: { id: @user_1.id }
+        expect(response).to redirect_to(root_path)
+      end  
+
     end  
 
   end
